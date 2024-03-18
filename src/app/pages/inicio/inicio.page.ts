2258985ab07f3b770454a43d0405e2a921b2337d
Kevin <keviropra@hotmail.com>
2024-03-17 21:38:21 -0400

    fin de los cambios

---
+++ src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Componente{
+  icon:string;
+  name:string;
+  redirecTo:string;
+}
+
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+})
+export class InicioPage implements OnInit {
+
+  componentes: Componente[]=[
+    {
+      icon: 'american-football',
+      name: 'Action-Sheet',
+      redirecTo: '/action-sheet'
+    },
+    {
+      icon: 'logo-apple-appstore',
+      name: 'Alert',
+      redirecTo: '/alert'
+    }
+
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
